e88ffa29f3e993c73ffe2b5267dfffd127dbd7d0
kxaguiar <kauaroaguiar16@gmail.com>
2025-12-15 18:39:37 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://formativa-front-faculdade.onrender.com/api', 
+  baseURL: 'https://sysmaintenance-1.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
